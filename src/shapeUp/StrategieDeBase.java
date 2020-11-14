8f68d7cb93287313b78dc24d0544eaed3cd5e628
Irwina127 <mzarzelli1@gmail.com>
2020-11-14 11:47:49 +0100

    distribuerCartes SdB

--- src/shapeUp/StrategieDeBase.java
+++ src/shapeUp/StrategieDeBase.java
@@ -1,5 +1,6 @@
 package shapeUp;
 
+import java.util.Iterator;
 import java.util.Random;
 
 public class StrategieDeBase implements StrategieMode {
@@ -10,38 +11,34 @@ public class StrategieDeBase implements StrategieMode {
     	
 		int nbreDeJoueurs = maPartie.getListeJoueurs().size();
 		
+		Iterator<String> iteratorRecup = maPartie.getListeJoueurs().keySet().iterator();
+
+		while(iteratorRecup.hasNext())  {
+			
+		  //Paramètres pour récupérer une carte au hasard
+			
+		  int longueurListeCarte = maPartie.getCarteDuJeu().size();
+  		  int randomIndex = new Random().nextInt(longueurListeCarte);
+  		  
+  		  //Récupérer la carte au hasard    
+  		  CarteJouable carteJoueur = new CarteJouable(null,null,false);
+  		  carteJoueur = (CarteJouable) maPartie.getCarteDuJeu().get(randomIndex);
+  		  
+  		  //Récupérer les différents joueurs pour leur attribuer une carte 
+  		  Object key   = iteratorRecup.next();
+  		  maPartie.getListeJoueurs().get(key).setMain(carteJoueur);
+  		  
+  		  //Réinitialisation des paramètres pour récuperer une carte au hasard
+  		  
+  		  longueurListeCarte = 0;
+  		  randomIndex = 0;
+		}
+		
+		
     	if (nbreDeJoueurs == 2) {
-
-    		
-    		int longueurListeCarte = maPartie.getCarteDuJeu().size();
-    		int randomIndex = new Random().nextInt(longueurListeCarte);
-    	
-    		CarteJouable carteJ1 = new CarteJouable();
-    		
-    		carteJ1 = (CarteJouable) maPartie.getCarteDuJeu().get(randomIndex);
-    		joueur.setMain(carteJ1);
-    		
-    		longueurListeCarte = 0;
-    		randomIndex = 0;
-    		longueurListeCarte = maPartie.getCarteDuJeu().size();
-    		randomIndex = new Random().nextInt(longueurListeCarte);
-
-    		
-    		CarteJouable carteJ2 = new CarteJouable();
-    		
-    		carteJ2 = (CarteJouable) maPartie.getCarteDuJeu().get(randomIndex);
-    		
-    		//maPartie.getListeJoueurs()
-    		//.setMain(carteJ2);
-    		
-    		
     		Pioche pioche = new Pioche (15, maPartie);
     		
     	} else if (nbreDeJoueurs == 3 ) {
- 
-    	
-    	
-    		
     		Pioche pioche= new Pioche (14, maPartie);
     	}
     	
