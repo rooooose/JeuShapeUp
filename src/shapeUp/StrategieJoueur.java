033ff4f76a02a292e2d9e68e2814f99f660e4734
Shir Frank <shir.frank@etu.univ-smb.fr>
2020-12-11 23:06:36 +0100

    Update StrategieJoueur.java

--- src/shapeUp/StrategieJoueur.java
+++ src/shapeUp/StrategieJoueur.java
@@ -5,13 +5,13 @@ public interface StrategieJoueur {
 	
     public Carte piocherCarte(Pioche pioche);
 
-    //public Carte definirCarteAJouer(Joueur joueur, StrategieMode modeDeJeu);
+    public Carte definirCarteAJouer(Joueur joueur, StrategieMode modeDeJeu);
     
-//    public int choisirLigneCarte(TapisDeJeu tapis);
-//	
-//	public int choisirColonneCarte(TapisDeJeu tapis);
-//	
-//	public boolean proposerDeplacement(TapisDeJeu tapis);
+    public int choisirLigneCarte(TapisDeJeu tapis);
+	
+	public int choisirColonneCarte(TapisDeJeu tapis);
+	
+	public boolean proposerDeplacement(TapisDeJeu tapis);
     
     public void deplacerCarte(TapisDeJeu tapis);
     
